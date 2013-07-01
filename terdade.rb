e510c1d5b16e42f52c925cb4e6fda1f172137075
Kyle Johnson <kyle@vacantminded.com>
2013-07-01 16:21:56 +0000

    terdade: refactor, add great/grate

--- terdade.rb
+++ terdade.rb
@@ -1,11 +1,9 @@
-event :PRIVMSG do
+need_module! 'regex_handler'
+
+regex /r+e+t+a+r+d+e+d+/i do
   unless @msg.connection.name == :ponychat and @msg.destination_canon == "#AUTISTIC-FAGGOTS"
     next
   end
-
-  match = @msg.stripped.match(/(?<word>r+e+t+a+r+d+e+d+)/i)
-
-  next unless match
 
   match = @msg.stripped.match(/\01ACTION (?<text>.+)\01/)
 
@@ -14,6 +12,23 @@ event :PRIVMSG do
     reply "* #{@msg.nick} #{text}", false
   else
     text = @msg.stripped.gsub(/(r+)(e+)(t+)(a+)(r+)(d+)(e+)(d+)/i, '\3\2\1\6\4\8\7')
+    reply "<#{@msg.nick}> #{text}", false
+  end
+
+end
+
+regex /g+r+e+a+t+/i do
+  unless @msg.connection.name == :ponychat and @msg.destination_canon == "#AUTISTIC-FAGGOTS"
+    next
+  end
+
+  match = @msg.stripped.match(/\01ACTION (?<text>.+)\01/)
+
+  if match
+    text = match[:text].gsub(/(g+)(r+)(e+)(a+)(t+)/i, '\1\2\4\5\3')
+    reply "* #{@msg.nick} #{text}", false
+  else
+    text = @msg.stripped.gsub(/(g+)(r+)(e+)(a+)(t+)/i, '\1\2\4\5\3')
     reply "<#{@msg.nick}> #{text}", false
   end
 
