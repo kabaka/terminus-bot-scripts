5b444f6ef2ec8aa6d9044777ea0f01abdaf8edb3
Kyle Johnson <kyle@vacantminded.com>
2013-07-02 00:51:29 +0000

    terdade: refactor

--- terdade.rb
+++ terdade.rb
@@ -1,35 +1,23 @@
 need_module! 'regex_handler'
 
-regex /r+e+t+a+r+d+e+d+/i do
-  unless @msg.connection.name == :ponychat and @msg.destination_canon == "#AUTISTIC-FAGGOTS"
-    next
-  end
+regex /r+e+t+a+r+d+e+d+|g+r+e+a+t+/i do
+  next unless @msg.connection.name   == :ponychat 
+  next unless @msg.destination_canon == "#AUTISTIC-FAGGOTS"
 
   match = @msg.stripped.match(/\01ACTION (?<text>.+)\01/)
 
   if match
-    text = match[:text].gsub(/(r+)(e+)(t+)(a+)(r+)(d+)(e+)(d+)/i, '\3\2\1\6\4\8\7')
-    reply "* #{@msg.nick} #{text}", false
+    reply "* #{@msg.nick} #{apply_substitutions match[:text]}", false
   else
-    text = @msg.stripped.gsub(/(r+)(e+)(t+)(a+)(r+)(d+)(e+)(d+)/i, '\3\2\1\6\4\8\7')
-    reply "<#{@msg.nick}> #{text}", false
+    reply "<#{@msg.nick}> #{apply_substitutions @msg.stripped}", false
   end
 
 end
 
-regex /g+r+e+a+t+/i do
-  unless @msg.connection.name == :ponychat and @msg.destination_canon == "#AUTISTIC-FAGGOTS"
-    next
+helpers do
+  def apply_substitutions text
+    text.gsub! /(g+)(r+)(e+)(a+)(t+)/i,             '\1\2\4\5\3'
+    text.gsub! /(r+)(e+)(t+)(a+)(r+)(d+)(e+)(d+)/i, '\3\2\1\6\4\8\7'
   end
-
-  match = @msg.stripped.match(/\01ACTION (?<text>.+)\01/)
-
-  if match
-    text = match[:text].gsub(/(g+)(r+)(e+)(a+)(t+)/i, '\1\2\4\5\3')
-    reply "* #{@msg.nick} #{text}", false
-  else
-    text = @msg.stripped.gsub(/(g+)(r+)(e+)(a+)(t+)/i, '\1\2\4\5\3')
-    reply "<#{@msg.nick}> #{text}", false
-  end
-
 end
+
